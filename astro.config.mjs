bbe259c141fd599bc521c4a469a7807670315eec
DAMIEN-PARK <damien.park.personal@gmail.com>
2025-04-16 00:27:15 +0900

    Update Astro config for static Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mywebpage-bay.vercel.app',  // Vercel 배포 URL
   integrations: [mdx(), sitemap(), tailwind()],
-  output: 'server',  // Vercel의 서버리스 환경에 맞게 설정
-  adapter: vercel(),
+  output: 'static',  // 정적 사이트로 빌드
+  adapter: vercel({
+    analytics: true,  // Vercel 애널리틱스 활성화
+  }),
 });
