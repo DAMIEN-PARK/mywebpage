d33c5440b2bfbe031c22e124f3dcd52d40fac03a
DAMIEN-PARK <damien.park.personal@gmail.com>
2025-04-16 00:29:19 +0900

    Fix content collection structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mywebpage-bay.vercel.app',  // Vercel 배포 URL
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: 'static',  // 정적 사이트로 빌드
-  adapter: vercel({
-    analytics: true,  // Vercel 애널리틱스 활성화
-  }),
+  output: 'server',
+  adapter: vercel(),
+  integrations: [mdx(), tailwind()]
 });
