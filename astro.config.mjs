664360619f31f751dd4c4ca0c8bf85c78419ea6b
DAMIEN-PARK <damien.park.personal@gmail.com>
2025-04-16 01:37:56 +0900

    fix: simplify configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,27 +6,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mywebpage-bay.vercel.app',
-  output: 'server',
-  adapter: vercel({
-    analytics: true,
-    webAnalytics: {
-      enabled: true
-    },
-    imageService: true,
-    devImageService: 'sharp',
-    imagesConfig: {
-      sizes: [640, 750, 828, 1080, 1200, 1920],
-      domains: [],
-      formats: ['image/avif', 'image/webp'],
-    },
-  }),
-  integrations: [mdx(), tailwind()],
-  markdown: {
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'github-dark',
-      langs: [],
-    },
-  },
+  output: 'hybrid',
+  adapter: vercel(),
+  integrations: [mdx(), tailwind()]
 });
